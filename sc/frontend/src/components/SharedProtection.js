09301bc4e47ad897aec793ff2e341a98c1dcf7bb
MaitreyeeSharma <128071427+MaitreyeeSharma@users.noreply.github.com>
2024-09-12 10:01:27 +0530

    Add files via upload

---
+++ sc/frontend/src/components/SharedProtection.js
@@ -0,0 +1,19 @@
+
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import Loading from "./Loading";
+import React from 'react';
+
+
+export const SharedProtection = ({ children, role }) => {
+  const { user, userLoading } = useAppContext();
+
+  console.log(user);
+  console.log(userLoading);
+
+  if (userLoading) return <Loading />;
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+  return children;
+};
